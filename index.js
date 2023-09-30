efbf41426079f3bd020e5e39ea753b4caed0b997
Jamie-19 <jamesjoseph190403@gmail.com>
2023-09-30 23:35:12 +0530

    .

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let name = 'John Doe';
 
 document.getElementById("p1").innerHTML = "Hello World";
 document.getElementById("p2").innerHTML = "Your age is "+ age;
-document.getElementById("p3").innerHTML = "Your name is "+ name;*/
+document.getElementById("p3").innerHTML = "Your name is "+ name;
 
 let username;
 
@@ -15,3 +15,22 @@ document.getElementById("myButton").onclick = function() {
     document.getElementById("myLabel").innerHTML = "Hello " + username;
     document.getElementById("p1").innerHTML = "The name is " + username;
 }
+
+let x;
+let y;
+let z;
+
+
+x = 10;
+y="james";
+z=true;
+
+console.log(typeof x);
+console.log(typeof y);
+console.log(typeof z);
+
+y=Number(y);
+x=String(x);
+console.log(typeof y);
+console.log(typeof x);
+console.log(x).value;*/
